66a748cb33ec0b168e589c6f332982c6eb5cdca4
iaxmedov <isaakahmedov@gmail.com>
2022-10-31 07:56:49 +0500

    added all

--- backend/views/layouts/_footer.php
+++ backend/views/layouts/_footer.php
@@ -2,6 +2,7 @@
     <div class="float-right d-none d-sm-block">
         <b>Version</b> 2.0
     </div>
-    <strong>Copyright &copy; 2020-<?= date('Y') ?> <a href="https://netsoft.uz">NETSOFT</a>.</strong>
+<!--    <strong>Copyright &copy; 2020---><?//= date('Y') ?><!-- <a href="https://netsoft.uz">NETSOFT</a>.</strong>-->
+    <strong>Copyright &copy; 2020-<?= date('Y') ?> Isxoqjon Axmedov.</strong>
     All rights reserved.
 </footer>
