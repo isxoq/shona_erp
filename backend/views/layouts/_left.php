56c3ecacc5904d5603a0544c3d8fedf75c142113
iaxmedov <isaakahmedov@gmail.com>
2022-12-09 20:36:22 +0500

    added expensese

--- backend/views/layouts/_left.php
+++ backend/views/layouts/_left.php
@@ -27,10 +27,9 @@ $menuItems = [
     ['label' => "Oylik maosh", 'url' => ['/staff/salary'], 'icon' => 'money-bill-wave', "visible" => !Yii::$app->user->identity->checkRoles(["Administrator", "Rahbar"])],
     ['label' => "Oylik maosh", 'url' => ['/staff/total-salary'], 'icon' => 'money-bill-wave', "visible" => Yii::$app->user->identity->checkRoles(["admin", "Administrator", "Rahbar"])],
     ['label' => "Qarzdorlik", 'url' => ['/debt/index'], 'icon' => 'money-check-alt', "visible" => Yii::$app->user->identity->checkRoles(["admin", "Administrator", "Rahbar"])],
-    ['label' => "Harajatlar", 'icon' => 'money-check-alt', "visible" => Yii::$app->user->identity->checkRoles(["admin", "Administrator", "Rahbar"]), "items" => [
+    ['label' => "Harajatlar", 'icon' => 'money-check', "visible" => Yii::$app->user->identity->checkRoles(["admin", "Administrator", "Rahbar"]), "items" => [
         ['label' => "Ro'yhat", 'url' => ['/expenses/index'], 'icon' => 'list',],
         ['label' => "Harajat turlari", 'url' => ['/expense-types/index'], 'icon' => 'list',],
-
     ]],
     ['label' => "Translations", 'url' => ['/translate-manager'], 'icon' => 'list', "visible" => Yii::$app->user->identity->checkRoles(["admin"])],
     ['label' => "Gii", 'url' => ['/gii'], 'icon' => 'code,fas', 'visible' => YII_DEBUG],
